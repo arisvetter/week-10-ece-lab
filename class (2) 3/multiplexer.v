20520480febd9851f35ad672a38df6e5052410c9
arisvetter <142842842+arisvetter@users.noreply.github.com>
2025-10-28 16:03:23 +0000

    mux demux

---
+++ class (2) 3/multiplexer.v
@@ -0,0 +1,19 @@
+module multiplexer(
+    input [1:0] sel,
+    input reg [7:0] A,
+    input reg [7:0] B,
+    input reg [7:0] C,
+    input reg [7:0] D
+    output [7:0] data,
+);
+
+    always @(*) begin 
+        case(sel)
+            2'b00: {D, C, B, A} <= {8'b0, 8'b0, 8'b0, data}; 
+            2'b01: {D, C, B, A} <= {8'b0, 8'b0, data, 8'b0};
+            2'b10: {D, C, B, A} <= {8'b0, data, 8'b0, 8'b0};
+            2'b11: {D, C, B, A} <= {data, 8'b0, 8'b0, 8'b0};
+        endcase
+    end
+
+endmodule
